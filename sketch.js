04924e65d9534df1b48f716dc71b747a311b570b
damianogui <damianogui@gmail.com>
2017-11-21 21:13:47 +0100

    Class example

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+let bubble;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	bubble = new Bubble(200,200,40);
 }
 
 function draw() {
-	background(0,255,0);
+	background(0,0,255);
+	//bubble.move();
+	bubble.show();
+}
+
+class Bubble {
+	constructor (x,y,r){
+		this.x = x;
+		this.y = y;
+		this.r = r;
+	}
+	move(){
+		this.x = this.x + random(-3,+3);
+		this.y = this.y + random(-3,+3);
+	}
+	show(){
+		fill(255,0,0);
+		ellipse(this.x,this.y,this.r,this.r);
+	}
 }
